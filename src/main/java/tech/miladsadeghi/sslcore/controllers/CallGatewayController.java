a7b13673f702410fd8f644da3f547de6df7d9d22
Milad Sadeghi <miladsadeghidm@gmail.com>
2026-02-05 23:21:57 +0330

    Creating and implementing CallGatewayController

---
+++ src/main/java/tech/miladsadeghi/sslcore/controllers/CallGatewayController.java
@@ -0,0 +1,15 @@
+package tech.miladsadeghi.sslcore.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CallGatewayController {
+
+    @GetMapping("/call-gateway")
+    public ResponseEntity<String> callGateway() {
+        return new ResponseEntity<>("Gateway server has been called.", HttpStatus.OK);
+    }
+}
