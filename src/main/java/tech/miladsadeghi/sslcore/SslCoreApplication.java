6dd4655585ab6cbaed1b92237546947dc67f9ed2
Milad Sadeghi <miladsadeghidm@gmail.com>
2026-02-05 22:15:34 +0330

    Initial commit

---
+++ src/main/java/tech/miladsadeghi/sslcore/SslCoreApplication.java
@@ -0,0 +1,13 @@
+package tech.miladsadeghi.sslcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SslCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SslCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/miladsadeghi/sslcore/SslCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.miladsadeghi.sslcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SslCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
